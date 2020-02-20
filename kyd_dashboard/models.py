60e88b2f9f9a17c77f4623bcead9ff857318aa45
AnimeshN <animesh.nauti@gmail.com>
2020-02-20 07:01:46 +0000

    feature 1

--- kyd_dashboard/models.py
+++ kyd_dashboard/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+
+
 class F1(models.Model):
-    id = models.AutoField(primary_key=True)
     month = models.CharField(max_length=10, blank=True, null=True)
     block = models.CharField(max_length=25, blank=True, null=True)
     weightp = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
--- kyd_dashboard/views.py
+++ kyd_dashboard/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
+from .models import F1
+from django.core import serializers
 # Create your views here.
 
 class KYDDashboardView(TemplateView):
@@ -8,7 +9,10 @@ class KYDDashboardView(TemplateView):
 
 
 class FeatureOne(TemplateView):
-    template_name = "kyd_dashboard/feature1.html"
+    def get(self,request):
+            data = F1.objects.all()
+            jsondata = serializers.serialize('json',data)
+            return render(request,'kyd_dashboard/feature1.html',{"data":jsondata})
 
 class FeatureTwo(TemplateView):
     template_name = "kyd_dashboard/feature2.html"
