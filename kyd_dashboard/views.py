e89c9a0c7238f7dbc14af8b9a40888011fbad2e0
AnimeshN <animesh.nauti@gmail.com>
2020-02-20 09:53:51 +0000

    feat 1

--- kyd_dashboard/views.py
+++ kyd_dashboard/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import F1
 from django.core import serializers
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 # Create your views here.
 
 class KYDDashboardView(TemplateView):
     template_name = "kyd_dashboard/kyd_base.html"
 
-
-class FeatureOne(TemplateView):
+class FeatureOne(LoginRequiredMixin,TemplateView):
+    login_url = '/login/'
     def get(self,request):
             data = F1.objects.all()
             jsondata = serializers.serialize('json',data)
