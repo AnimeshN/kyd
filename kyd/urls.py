6cf096cd3c40075b143d09c4ca9b0f48f8fb91f9
AnimeshN <animesh.nauti@gmail.com>
2020-03-22 16:37:34 +0000

    feature 7

--- kyd/urls.py
+++ kyd/urls.py
@@ -50,8 +50,6 @@ urlpatterns = [
     url(r'^maha_dashboard/maha_feature5$', MahaFeatureFive.as_view(), name='maha-feat5'), #radar2
 
 
-
-
     url(r'^admin/', admin.site.urls),
     url(r'^login/', views.login_request, name='login'),
     url(r'^logout/', views.logout_request, name='logout'),
